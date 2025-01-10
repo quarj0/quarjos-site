ee0202848cf3fec9632ad9bc618b76977fe56bbc
quarj0 <owusuansahkwadwo24@gmail.com>
2025-01-10 22:01:45 +0000

    fix: adjust heading size and image width in Homepage component for better layout

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -5,8 +5,8 @@ const Homepage = () => {
   return (
     <div className="min-h-screen bg-gradient-to-r from-black via-gray-900 to-purple-950 text-white flex flex-col">
       <div className="flex-1 flex flex-col md:flex-row items-center justify-evenly px-6 md:px-12">
-        <div className="max-w-lg text-center md:text-left">
-          <h1 className="text-4xl md:text-6xl font-bold leading-tight mb-4">
+        <div className="max-w-lg text-center md:text-left mt-5">
+          <h1 className="text-3xl md:text-6xl font-bold leading-tight mb-4 sm:mb-6 sm:mt-8">
             I&apos;m Owusu Ansah
           </h1>
           <p className="text-blue-400 text-xs md:text-sm uppercase tracking-widest mb-6">
@@ -25,7 +25,7 @@ const Homepage = () => {
           <img
             src={MyImage}
             alt="owusu's picture"
-            className="w-[200px] sm:w-[300px] md:w-[400px] lg:w-[600px] h-auto object-cover rounded-full shadow-lg"
+            className="w-[300px] sm:w-[300px] md:w-[400px] lg:w-[600px] h-auto object-cover rounded-full shadow-lg"
           />
         </div>
       </div>
